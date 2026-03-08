e4863b60dc3a546a34475bd80fc5308a03daa966
alevy <andrei.levchenko.ie@gmail.com>
2026-03-08 15:47:16 +0000

    add stub http client

--- lab5_services/src/app/app.ts
+++ lab5_services/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Students } from "./students/students";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Students],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- lab5_services/src/app/services/student-service.ts
+++ lab5_services/src/app/services/student-service.ts
@@ -1,11 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StudentService {
   
-  constructor() {}
+  constructor(public httpClient: HttpClient) {}
 
   getStudentData() {
     return [

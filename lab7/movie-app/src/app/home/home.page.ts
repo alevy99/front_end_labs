97d17203cd7872939c8d1007291ff9e063168f47
alevy <andrei.levchenko.ie@gmail.com>
2026-03-28 22:20:51 +0000

    comments

--- lab7/movie-app/src/app/home/home.page.ts
+++ lab7/movie-app/src/app/home/home.page.ts
@@ -9,15 +9,16 @@ import { MovieService } from '../Services/movie-service';
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonImg, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent],
 })
 export class HomePage {
+  // Inject the MovieService to fetch movie data
   constructor(private movieService: MovieService) {}
 
   movies: any[] = [];
 
   ionViewWillEnter() {
+    // Fetch movies when the view is about to enter
     this.movieService.getMovies().subscribe((data) => {
-      
       this.movies = data.Search; // Assuming the API returns a 'Search' array
-      console.log(this.movies);
+      console.log(this.movies); // log the movies to verify the data is being fetched correctly
     });
   }
 }
